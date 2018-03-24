41620382b291b79c10025d2ea7d514fc23fe8147
aimanhebat <ilovebsod@gmail.com>
2018-03-24 17:55:35 +0800

    Styling using Milligram CSS

--- index.php
+++ index.php
@@ -23,34 +23,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Whatsapp Dox</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <script src="main.js"></script> -->
-
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v2.3.7/dist/mini-default.min.css">
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-<div class="pure-g">
-    <div class="pure-u-1-3"></div>
-    <div class="pure-u-1-3">
-        
-        <div>
-            <h2>Whatsapp <img width="30px" style="vertical-align:middle" src="whatsapp-logo.png"> Dox</h2>
-            </div>
-        <hr>
-            
-        <p><small><strong>Note:</strong> Alpha version. Malaysia number only. Country code (+6) is not required</small></p>
-        <p><small><a target="_blank" href="https://github.com/aimanbaharum/whatsapp-dox">Source</a></small></p>
-        
-        <form class="pure-form pure-form-stacked" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <fieldset>
-            
-                <label for="number">Number</label>
-                <input type="number" id="number" name="number" placeholder="0132746273">
-                <span class="pure-form-message">This is a required field.</span>
+<div class="container">
+    <div>
+        <h1>Whatsapp <img width="30px" style="vertical-align:middle" src="whatsapp-logo.png"> Dox</h1>
+        </div>
+    <hr>
 
-                <button type="submit" class="pure-button" name="submit">Submit</button>
-            </fieldset>
-        </form>
-    </div>
-    <div class="pure-u-1-3"></div>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <p><small><strong>Note:</strong> Alpha version. Malaysia number only. Country code (+6) is not required</small></p>
+        <fieldset>
+        
+            <label for="phoneNumber">Number</label>
+            <input type="number" id="phoneNumber" name="number" placeholder="0132746273">
+
+            <button type="submit" name="submit">Submit</button>
+        </fieldset>
+    </form>
+
+    <p><small><a target="_blank" href="https://github.com/aimanbaharum/whatsapp-dox">Source</a></small></p>
 </div>
 
 </body>
