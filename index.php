42a93c7d4a213f6ce166fe548a9b80ea9bd7ccda
aimanhebat <ilovebsod@gmail.com>
2018-03-24 19:36:36 +0800

    Fix typo

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
 
 	<meta name="description" content="Whatsapp phone number lookup" />
-    <link rel="shortcut icon" href="/favicon-32x32.png" type="image/png" />
+    <link rel="shortcut icon" href="favicon-32x32.png" type="image/png" />
     <link rel="canonical" href="http://aimanbaharum.com/whatsapp-dox" />
     <meta name="referrer" content="no-referrer-when-downgrade" />
     
